4f98f27ef098811a2e43a3b2046c57d4a78ad5ae
Alex Cipollone <alex.cipollone10@gmail.com>
2024-02-26 18:35:48 -0500

    completed business logic section

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -3,9 +3,18 @@ package com.example.demo.student;
 // Now starting the data layer
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     // Long corresponds with our declared ID
+
+    // Transforms into SQL
+    // SELECT * FROM student WHERE...
+    // OR - the below is "JPQL"
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
+
