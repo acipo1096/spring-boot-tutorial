17074df2776d47c65467c41f597773cd5b4d4c9e
Alex Cipollone <alex.cipollone10@gmail.com>
2024-03-01 12:05:03 -0500

    tutorial complete

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -33,10 +33,12 @@ public class StudentController {
         studentService.addNewStudent(student);
     }
 
-    // PUT exercise
-    @Transactional
-    public void updateStudent(@PathVariable("studentId") Long studentId) {
-        studentService.updateStudent(studentId);
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
     }
 
     @DeleteMapping(path = "{studentId}")
